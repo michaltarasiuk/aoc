22e2a9216affde177683d51492796b8a371341cd
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2024-05-09 10:18:54 +0200

    2015/05 - part 1

---
+++ 2015/day_05.ts
@@ -0,0 +1,25 @@
+import {add} from 'lib/add';
+import {getInputLines} from 'lib/get_input';
+
+const lns = await getInputLines({
+	year: 2015,
+	day: 5,
+});
+
+{
+	const containsAtLeastThreeVowels = (string: string) =>
+		/(.*[aeuio].*){3}/.test(string);
+
+	const containsRepeatedLetters = (string: string) =>
+		/(?:(\w)\1+)/.test(string);
+
+	const containsAllowedSubstrings = (string: string) =>
+		!/ab|cd|pq|xy/.test(string);
+
+	const isNiceString = (string: string) =>
+		containsAtLeastThreeVowels(string) &&
+		containsRepeatedLetters(string) &&
+		containsAllowedSubstrings(string);
+
+	console.log(add(...lns.map(isNiceString)));
+}
--- lib/add.ts
+++ lib/add.ts
@@ -1,3 +1,4 @@
-export function add(...ns: number[]) {
-	return ns.reduce((a, b) => a + b, 0);
+export function add(...vals: (number | boolean)[]) {
+	// @ts-ignore -- Non-string, non-BigInt values are coerced to numbers.
+	return vals.reduce((a, b) => a + b, 0);
 }
