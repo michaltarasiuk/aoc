0c1cd14840803f102c6019d5d549692f2ebbff3f
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2024-05-12 20:20:14 +0200

    2015/08 - part 2

--- 2015/day_08.ts
+++ 2015/day_08.ts
@@ -10,3 +10,7 @@ const ns = lns.map(({length}) => length);
 const ns2 = (<string[]>eval(`[${lns.join()}]`)).map(({length}) => length);
 
 console.log(add(...ns) - add(...ns2));
+
+const ns3 = lns.map((ln) => JSON.stringify(ln).length);
+
+console.log(add(...ns3) - add(...ns));
