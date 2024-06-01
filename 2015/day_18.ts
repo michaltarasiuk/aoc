1be3ff46283fb98376d81ab7c662e4fe1eeda555
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2024-06-01 02:34:35 +0200

    2015/18#1

---
+++ 2015/day_18.ts
@@ -0,0 +1,38 @@
+import {getInputLns} from 'lib/input';
+
+const lns = await getInputLns({year: 2015, day: 18});
+
+const LIGHT_ON: string = '#';
+const LIGHT_OFF: string = '.';
+
+const getNeighbors = (state: string[][], {x, y}: {x: number; y: number}) => {
+	const start = Math.max(0, x - 1);
+	const end = x + 2;
+
+	return [state[y][x - 1], state[y][x + 1]]
+		.concat(state[y - 1]?.slice(start, end))
+		.concat(state[y + 1]?.slice(start, end));
+};
+
+const actions = {
+	[LIGHT_ON](neighbors: string[]) {
+		const {length} = neighbors.filter((n) => n === LIGHT_ON);
+		return length === 2 || length === 3 ? LIGHT_ON : LIGHT_OFF;
+	},
+	[LIGHT_OFF](neighbors: string[]) {
+		const {length} = neighbors.filter((n) => n === LIGHT_ON);
+		return length === 3 ? LIGHT_ON : LIGHT_OFF;
+	},
+};
+
+let state = lns.map((ln) => ln.split(''));
+
+for (let i = 0; i < 100; i++) {
+	state = state.map((row, y) =>
+		row.map((light, x) => actions[light](getNeighbors(state, {x, y}))),
+	);
+}
+
+const part1 = state.flat().filter((light) => light === LIGHT_ON).length;
+
+console.log(part1);
