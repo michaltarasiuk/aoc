3dbb9aea6707b178438cb52fc00ce7a3d4b23ee4
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2024-06-12 16:09:28 +0200

    2018/01#2

--- 2018/day_01.ts
+++ 2018/day_01.ts
@@ -3,4 +3,23 @@ import {sum} from 'lib/sum';
 
 const ns = await getInputNumbers({year: 2018, day: 1});
 
-console.log(sum(...ns));
+function findFirstFrequencyReachesTwice(...ns: number[]) {
+	let frequency = 0;
+	const frequencies = new Set<number>();
+
+	while (true) {
+		for (const n of ns) {
+			frequency += n;
+
+			if (frequencies.has(frequency)) {
+				return frequency;
+			}
+			frequencies.add(frequency);
+		}
+	}
+}
+
+const result = sum(...ns);
+const result2 = findFirstFrequencyReachesTwice(...ns);
+
+console.log({result, result2});
