b938178119ea7fdaa78ec4451d5e6b9fdd04c5cc
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2024-06-17 16:42:45 +0200

    refactor 2015/18

--- 2015/day_18.ts
+++ 2015/day_18.ts
@@ -2,46 +2,42 @@ import {getInputGrid} from 'lib/input';
 
 const grid = await getInputGrid({year: 2015, day: 18});
 
-const ENABLED_LIGHT = '#';
-const DISABLED_LIGHT = '.';
+const LIGHT_ON = '#';
+const LIGHT_OFF = '.';
 
-const NEIGHBORS = [
-	[-1, 0, 1],
-	[-1, 1],
-	[-1, 0, 1],
-];
+type Lights = string[][];
 
-function getNeighbors(lights: string[][], {x, y}: {x: number; y: number}) {
-	return NEIGHBORS.reduce<string[][]>((acc, offsets, i) => {
-		acc.push(
-			offsets.flatMap((offset) => lights[y + i - 1]?.[x + offset] ?? []),
-		);
-		return acc;
-	}, []);
+function getNeighbors(lights: Lights, {x, y}: {x: number; y: number}) {
+	const NEIGHBORS = [
+		[-1, 0, 1],
+		[-1, 1],
+		[-1, 0, 1],
+	];
+	return NEIGHBORS.map((offsets, i) =>
+		offsets.flatMap((offset) => lights[y + i - 1]?.[x + offset] ?? []),
+	);
 }
 
-function countEnabledLights(lights: string[][]) {
-	const enabledLightsRe = new RegExp(ENABLED_LIGHT, 'g');
-	return Array.from(lights.join().matchAll(enabledLightsRe)).length;
+function countLightsOn(lights: Lights) {
+	const lightsOnRe = new RegExp(LIGHT_ON, 'g');
+	return Array.from(lights.join().matchAll(lightsOnRe)).length;
 }
 
-function switchLight(light: string, enabledLightsCount: number) {
-	if (light === ENABLED_LIGHT) {
-		return enabledLightsCount === 2 || enabledLightsCount === 3
-			? ENABLED_LIGHT
-			: DISABLED_LIGHT;
+function switchLight(light: string, neighbors: Lights) {
+	const count = countLightsOn(neighbors);
+	switch (light) {
+		case LIGHT_ON:
+			return count === 2 || count === 3 ? LIGHT_ON : LIGHT_OFF;
+		case LIGHT_OFF:
+			return count === 3 ? LIGHT_ON : LIGHT_OFF;
+		default:
+			throw new Error('Invalid light');
 	}
-	return enabledLightsCount === 3 ? ENABLED_LIGHT : DISABLED_LIGHT;
 }
 
-function animate(lights: string[][]) {
+function animate(lights: Lights) {
 	return lights.map((row, y) =>
-		row.map((light, x) => {
-			const neighbors = getNeighbors(lights, {x, y});
-			const enabledLightsCount = countEnabledLights(neighbors);
-
-			return switchLight(light, enabledLightsCount);
-		}),
+		row.map((light, x) => switchLight(light, getNeighbors(lights, {x, y}))),
 	);
 }
 
@@ -51,7 +47,7 @@ for (let i = 0; i < 100; i++) {
 	lights = animate(lights);
 }
 
-const result = countEnabledLights(lights);
+const result = countLightsOn(lights);
 
 if (import.meta.vitest) {
 	const {test, expect} = import.meta.vitest;
