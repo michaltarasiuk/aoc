935e9de9e42860f49f62188d3fb6639f1b17140b
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2024-06-21 02:54:57 +0200

    2016/07#2

--- 2016/day_07.ts
+++ 2016/day_07.ts
@@ -2,19 +2,33 @@ import {getInputLns} from 'lib/input';
 
 const lns = await getInputLns({year: 2016, day: 7});
 
-const abbaRe = /(\w)((?!\1)\w)\2\1/;
-const squareBracketRe = /\[|\]/;
-
-const result = lns.reduce((acc, ln) => {
+function parseLn(ln: string) {
+	const squareBracketRe = /\[|\]/;
 	const parts = ln.split(squareBracketRe);
-	const {supernets = [], hypernets = []} = Object.groupBy(parts, (_, i) =>
+
+	return Object.groupBy(parts, (_, i) =>
 		i % 2 === 0 ? 'supernets' : 'hypernets',
 	);
+}
 
-	if (abbaRe.test(supernets.join(' ')) && !abbaRe.test(hypernets.join(' '))) {
+const abbaRe = /(\w)((?!\1)\w)\2\1/;
+
+const result = lns.reduce((acc, ln) => {
+	const {supernets = [], hypernets = []} = parseLn(ln);
+
+	if (abbaRe.test(supernets.join()) && !abbaRe.test(hypernets.join())) {
 		acc++;
 	}
 	return acc;
+}, 0);
+
+const abaRe = /supernets: .*(\w)((?!\1)\w)\1.*, hypernets: .*\2\1\2.*/;
+
+const result2 = lns.reduce((acc, ln) => {
+	const {supernets = [], hypernets = []} = parseLn(ln);
+	const ip = `supernets: ${supernets}, hypernets: ${hypernets}`;
+
+	return acc + +abaRe.test(ip);
 }, 0);
 
 if (import.meta.vitest) {
@@ -22,5 +36,9 @@ if (import.meta.vitest) {
 
 	test('part 1', () => {
 		expect(result).toBe(110);
+	});
+
+	test('part 2', () => {
+		expect(result2).toBe(242);
 	});
 }
