0fbd0cf7d19ea7b497459cfb1fa37a466ef04541
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2024-06-22 20:38:30 +0200

    2017/02

---
+++ 2017/day_02.ts
@@ -0,0 +1,37 @@
+import {getInputLns} from 'lib/input';
+
+const lns = await getInputLns({year: 2017, day: 2});
+
+function parseLn(ln: string) {
+	return ln.split('\t').map(Number);
+}
+
+const result = lns.reduce((acc, ln) => {
+	const ns = parseLn(ln);
+	return acc + Math.max(...ns) - Math.min(...ns);
+}, 0);
+
+const result2 = lns.reduce((acc, ln) => {
+	const ns = parseLn(ln);
+
+	for (const n of ns) {
+		for (const m of ns) {
+			if (n !== m && n % m === 0) {
+				return acc + n / m;
+			}
+		}
+	}
+	return acc;
+}, 0);
+
+if (import.meta.vitest) {
+	const {test, expect} = import.meta.vitest;
+
+	test('part 1', () => {
+		expect(result).toBe(44216);
+	});
+
+	test('part 2', () => {
+		expect(result2).toBe(320);
+	});
+}
