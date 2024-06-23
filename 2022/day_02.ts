bbdf42fc8346831972389c7720f8a40451e55c96
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2024-06-23 16:29:05 +0200

    2022/02#2

--- 2022/day_02.ts
+++ 2022/day_02.ts
@@ -3,11 +3,15 @@ import {getInputLns} from 'lib/input';
 
 const lns = await getInputLns({year: 2022, day: 2});
 
-const ROCK = 1;
-const PAPER = 2;
-const SCISSORS = 3;
+const HAND_SHAPE = [1, 2, 3];
+const [ROCK, PAPER, SCISSORS] = HAND_SHAPE;
 
-const HAND_SHAPE = [ROCK, PAPER, SCISSORS];
+const [WIN, DRAW, LOSE] = HAND_SHAPE.toReversed();
+const POINTS = {
+	WIN: 6,
+	DRAW: 3,
+	LOSE: 0,
+};
 
 const THEM_CHARS: Record<string, number> = {
 	A: ROCK,
@@ -29,10 +33,25 @@ function parseLn(ln: string) {
 }
 
 function roundOutcome(them: number, me: number) {
-	if (them === me) return 3;
+	if (them === me) return POINTS.DRAW;
 
-	const beat = adjacentAt(HAND_SHAPE, HAND_SHAPE.indexOf(me))[1];
-	return them === beat ? 0 : 6;
+	const [lose] = adjacentAt(HAND_SHAPE, HAND_SHAPE.indexOf(me));
+	return them === lose ? POINTS.WIN : POINTS.LOSE;
+}
+
+function roundOutcome2(them: number, me: number) {
+	const [lose, beat] = adjacentAt(HAND_SHAPE, HAND_SHAPE.indexOf(them));
+
+	switch (me) {
+		case WIN:
+			return beat + POINTS.WIN;
+		case DRAW:
+			return them + POINTS.DRAW;
+		case LOSE:
+			return lose;
+		default:
+			throw new Error('Invalid hand shape');
+	}
 }
 
 const result = lns.reduce((acc, ln) => {
@@ -40,10 +59,19 @@ const result = lns.reduce((acc, ln) => {
 	return acc + roundOutcome(them, me) + me;
 }, 0);
 
+const result2 = lns.reduce((acc, ln) => {
+	const {them, me} = parseLn(ln);
+	return acc + roundOutcome2(them, me);
+}, 0);
+
 if (import.meta.vitest) {
 	const {test, expect} = import.meta.vitest;
 
 	test('part 1', () => {
 		expect(result).toBe(12855);
+	});
+
+	test('part 2', () => {
+		expect(result2).toBe(13726);
 	});
 }
