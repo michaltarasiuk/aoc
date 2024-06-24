c872d1ae996376877a15ba0c1a5596b0974cd38f
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2024-06-24 20:20:16 +0200

    refactor 2022/02

--- 2022/day_02.ts
+++ 2022/day_02.ts
@@ -1,75 +1,37 @@
-import {adjacentAt} from 'lib/adjacent_at';
 import {getInputLns} from 'lib/input';
 
 const lns = await getInputLns({year: 2022, day: 2});
 
-const HAND_SHAPE = [1, 2, 3];
-const [ROCK, PAPER, SCISSORS] = HAND_SHAPE;
+const SHAPES_COUNT = 3;
+const POINTS = {WIN: 6, DRAW: 3, LOSE: 0};
 
-const [WIN, DRAW, LOSE] = HAND_SHAPE.toReversed();
-const POINTS = {
-	WIN: 6,
-	DRAW: 3,
-	LOSE: 0,
-};
+function getHandShape(char: string) {
+	return ('ABCXYZ'.indexOf(char) % SHAPES_COUNT) + 1;
+}
 
-const THEM_CHARS: Record<string, number> = {
-	A: ROCK,
-	B: PAPER,
-	C: SCISSORS,
-};
-
-const ME_CHARS: Record<string, number> = {
-	X: ROCK,
-	Y: PAPER,
-	Z: SCISSORS,
-};
-
-function parseLn(ln: string) {
+function parseRound(round: string) {
 	const roundRe = /^([ABC]) ([XYZ])$/;
-	const [, them, me] = roundRe.exec(ln)!;
+	const [, them, me] = roundRe.exec(round)!;
 
-	return {them: THEM_CHARS[them], me: ME_CHARS[me]};
+	return [getHandShape(them), getHandShape(me)] as const;
 }
 
 function roundOutcome(them: number, me: number) {
-	const [lose, beat] = adjacentAt(HAND_SHAPE, HAND_SHAPE.indexOf(me));
+	const diff = me - them;
 
-	switch (them) {
-		case lose:
-			return POINTS.WIN + me;
-		case me:
-			return POINTS.DRAW + me;
-		case beat:
-			return POINTS.LOSE + me;
-		default:
-			throw new Error('Invalid hand shape');
+	if (diff === 0) {
+		return POINTS.DRAW;
+	} else if (diff === 1 || diff === -2) {
+		return POINTS.WIN;
+	} else if (diff === -1 || diff === 2) {
+		return POINTS.LOSE;
 	}
-}
-
-function roundOutcome2(them: number, me: number) {
-	const [lose, beat] = adjacentAt(HAND_SHAPE, HAND_SHAPE.indexOf(them));
-
-	switch (me) {
-		case WIN:
-			return POINTS.WIN + beat;
-		case DRAW:
-			return POINTS.DRAW + them;
-		case LOSE:
-			return lose;
-		default:
-			throw new Error('Invalid hand shape');
-	}
+	throw new Error('Invalid round outcome');
 }
 
 const result = lns.reduce((acc, ln) => {
-	const {them, me} = parseLn(ln);
-	return acc + roundOutcome(them, me);
-}, 0);
-
-const result2 = lns.reduce((acc, ln) => {
-	const {them, me} = parseLn(ln);
-	return acc + roundOutcome2(them, me);
+	const [them, me] = parseRound(ln);
+	return acc + roundOutcome(them, me) + me;
 }, 0);
 
 if (import.meta.vitest) {
@@ -77,9 +39,5 @@ if (import.meta.vitest) {
 
 	test('part 1', () => {
 		expect(result).toBe(12855);
-	});
-
-	test('part 2', () => {
-		expect(result2).toBe(13726);
 	});
 }
