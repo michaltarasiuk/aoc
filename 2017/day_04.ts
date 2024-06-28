3ad5c7cd0f28a5b0682f909f60db11148ab1ac7c
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2024-06-29 00:38:44 +0200

    2017/04#2

--- 2017/day_04.ts
+++ 2017/day_04.ts
@@ -12,10 +12,26 @@ const result = lns.reduce((acc, ln) => {
 	return acc;
 }, 0);
 
+const result2 = lns.reduce((acc, ln) => {
+	const words = ln.split(' ');
+	const uniqueWords = new Set(
+		words.map(([...chars]) => chars.toSorted().join('')),
+	);
+
+	if (words.length === uniqueWords.size) {
+		acc++;
+	}
+	return acc;
+}, 0);
+
 if (import.meta.vitest) {
 	const {test, expect} = import.meta.vitest;
 
 	test('part 1', () => {
 		expect(result).toBe(455);
+	});
+
+	test('part 2', () => {
+		expect(result2).toBe(186);
 	});
 }
