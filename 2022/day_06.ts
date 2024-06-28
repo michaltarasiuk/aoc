dca77b4ede6d136b34a860249af4dfe317637e28
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2024-06-28 19:06:47 +0200

    2022/06#2

--- 2022/day_06.ts
+++ 2022/day_06.ts
@@ -2,20 +2,29 @@ import {getInput} from 'lib/input';
 
 const input = await getInput({year: 2022, day: 6});
 
-const MARKER_SIZE = 4;
-let result: number;
+function findLastIndexOfFirstMarker(markerSize: number) {
+	let result: number | undefined = undefined;
 
-for (const i of Array.from(input, (_, i) => i)) {
-	if (new Set(input.slice(i, i + MARKER_SIZE)).size === MARKER_SIZE) {
-		result = i + MARKER_SIZE;
-		break;
+	for (const i of Array.from(input, (_, i) => i)) {
+		if (new Set(input.slice(i, i + markerSize)).size === markerSize) {
+			result = i + markerSize;
+			break;
+		}
 	}
+	return result;
 }
+
+const result = findLastIndexOfFirstMarker(4);
+const result2 = findLastIndexOfFirstMarker(14);
 
 if (import.meta.vitest) {
 	const {test, expect} = import.meta.vitest;
 
 	test('part 1', () => {
 		expect(result).toBe(1343);
+	});
+
+	test('part 2', () => {
+		expect(result2).toBe(2193);
 	});
 }
