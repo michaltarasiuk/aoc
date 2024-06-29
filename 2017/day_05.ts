3ed0aaaf9876fa0283650568b43483982658eda1
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2024-06-29 15:17:29 +0200

    2017/05#2

--- 2017/day_05.ts
+++ 2017/day_05.ts
@@ -2,18 +2,33 @@ import {getInputInts} from 'lib/input';
 
 const ints = await getInputInts({year: 2017, day: 5});
 
-let offset = 0;
-let steps = 0;
+function howManyStepsToExit(
+	[...ints]: number[],
+	jump: (ints: number[], offset: number) => number,
+) {
+	let offset = 0;
+	let steps = 0;
 
-while (offset < ints.length) {
-	offset += ints[offset]++;
-	steps++;
+	while (offset < ints.length) {
+		offset += jump(ints, offset);
+		steps++;
+	}
+	return steps;
 }
+
+const result = howManyStepsToExit(ints, (ints, offset) => ints[offset]++);
+const result2 = howManyStepsToExit(ints, (ints, offset) =>
+	ints[offset] >= 3 ? ints[offset]-- : ints[offset]++,
+);
 
 if (import.meta.vitest) {
 	const {test, expect} = import.meta.vitest;
 
 	test('part 1', () => {
-		expect(steps).toBe(373160);
+		expect(result).toBe(373160);
+	});
+
+	test('part 2', () => {
+		expect(result2).toBe(26395586);
 	});
 }
