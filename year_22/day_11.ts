5a027e55fad4f4f95157a612ad60ffea609de001
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2024-07-24 12:30:50 +0200

    22/11#1

--- year_22/day_11.ts
+++ year_22/day_11.ts
@@ -25,15 +25,48 @@ function parseMonkey(paragraph: string[]) {
   return MONKEY_SCHEMA.parse(groups);
 }
 
-function evalOperation(operation: string, old: number) {
+function evalOperation(operation: string, old: number): number {
   return eval(operation.replaceAll(/old/g, String(old)));
 }
 
-const monkeys = paragraphs.map(parseMonkey);
+const monkeys = new Map(
+  paragraphs.map((p) => {
+    const monkey = parseMonkey(p);
+    return [monkey.id, {inspects: 0, ...monkey}];
+  }),
+);
 
-// TODO: Implement part 1 and part 2
-for (const monkey of monkeys) {
-  for (const item of monkey.items) {
-    evalOperation(monkey.operation, item);
+type Monkey = typeof monkeys extends Map<number, infer Monkey> ? Monkey : never;
+
+function monkeyAct(monkey: Monkey) {
+  while (monkey.items.length) {
+    const item = Math.floor(
+      evalOperation(monkey.operation, monkey.items.shift()!) / 3,
+    );
+    const divisible = item % monkey.divisibleBy === 0;
+    const throwTo = divisible ? monkey.throwToTrue : monkey.throwToFalse;
+
+    monkey.inspects++;
+    monkeys.get(throwTo)!.items.push(item);
   }
+}
+
+for (let i = 1; i <= 20; i++) {
+  for (const monkey of monkeys.values()) {
+    monkeyAct(monkey);
+  }
+}
+
+const [a, b] = Array.from(monkeys.values()).toSorted(
+  (a, b) => b.inspects - a.inspects,
+);
+
+const monkeyBussiness = a.inspects * b.inspects;
+
+if (import.meta.vitest) {
+  const {test, expect} = import.meta.vitest;
+
+  test('part 1', () => {
+    expect(monkeyBussiness).toBe(55458);
+  });
 }
