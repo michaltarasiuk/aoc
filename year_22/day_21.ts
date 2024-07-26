dfc3c76a01171a0f23da0ccb35775685cebca497
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2024-07-26 23:27:22 +0200

    22/21#1

---
+++ year_22/day_21.ts
@@ -0,0 +1,34 @@
+import {getInputLns} from 'lib/input';
+import {raise} from 'lib/raise';
+
+const jobs = await getInputLns({year: 2022, day: 21});
+
+const jobsMap = new Map(
+  jobs.map((job) => {
+    const jobRe = /^(\w{4}): (.+)$/;
+    const [, name, yell] = jobRe.exec(job)!;
+
+    return [name, yell];
+  }),
+);
+
+function workOutMonkeyNamed(name: string): number {
+  const yell = jobsMap.get(name) ?? raise(`No job for ${name}`);
+  const parsed = Number(yell);
+
+  if (Number.isNaN(parsed)) {
+    const [a, op, b] = yell.split(/\s/);
+    return eval(workOutMonkeyNamed(a) + op + workOutMonkeyNamed(b));
+  }
+  return parsed;
+}
+
+const yell = workOutMonkeyNamed('root');
+
+if (import.meta.vitest) {
+  const {test, expect} = import.meta.vitest;
+
+  test('part 1', () => {
+    expect(yell).toBe(142707821472432);
+  });
+}
