a2224ceb87c91c69bb02de8e4c5d8dc8041a8325
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2024-07-29 12:31:01 +0200

    23/08#2

--- lib/divisors.ts
+++ lib/divisors.ts
@@ -8,3 +8,19 @@ function* divisors_(n: number) {
 export function divisors(n: number) {
   return Array.from(divisors_(n));
 }
+
+function greatestCommonDivisor(a: number, b: number): number {
+  return b ? greatestCommonDivisor(b, a % b) : a;
+}
+
+export function gcd(...ns: number[]) {
+  return ns.reduce(greatestCommonDivisor);
+}
+
+function leastCommonMultiple(a: number, b: number) {
+  return a * (b / greatestCommonDivisor(a, b));
+}
+
+export function lcm(...ns: number[]) {
+  return ns.reduce(leastCommonMultiple);
+}
--- year_23/day_08.ts
+++ year_23/day_08.ts
@@ -1,3 +1,4 @@
+import {lcm} from 'lib/divisors';
 import {getInputParagraphs} from 'lib/input';
 
 const [[instructions], maps] = await getInputParagraphs({year: 2023, day: 8});
@@ -10,24 +11,53 @@ const parsedMaps = maps.reduce<Record<string, [string, string]>>((acc, map) => {
   return acc;
 }, {});
 
+function countStepsToNode(start: string, predicate: (node: string) => boolean) {
+  let node = start;
+  let steps = 0;
+
+  while (!predicate(node)) {
+    const [left, right] = parsedMaps[node];
+    const instruction = instructions[steps % instructions.length];
+
+    node = instruction === 'L' ? left : right;
+    steps++;
+  }
+  return steps;
+}
+
+function countStepsToNodes(
+  startingNodes: string[],
+  predicate: (node: string) => boolean,
+) {
+  const steps = startingNodes.map((start) =>
+    countStepsToNode(start, predicate),
+  );
+  return lcm(...steps);
+}
+
+function nodeEndsWith(node: string, char: string) {
+  return new RegExp(`^[A-Z]{2}${char}$`).test(node);
+}
+
 const START_NODE = 'AAA';
 const TARGET_NODE = 'ZZZ';
+const steps = countStepsToNode(START_NODE, (node) => node === TARGET_NODE);
 
-let node = START_NODE;
-let steps = 0;
-
-while (node !== TARGET_NODE) {
-  const instruction = instructions[steps % instructions.length];
-  const [left, right] = parsedMaps[node];
-
-  node = instruction === 'L' ? left : right;
-  steps++;
-}
+const nodesEndsWithA = Object.keys(parsedMaps).filter((node) =>
+  nodeEndsWith(node, 'A'),
+);
+const steps2 = countStepsToNodes(nodesEndsWithA, (node) =>
+  nodeEndsWith(node, 'Z'),
+);
 
 if (import.meta.vitest) {
   const {test, expect} = import.meta.vitest;
 
   test('part 1', () => {
     expect(steps).toBe(20569);
+  });
+
+  test('part 2', () => {
+    expect(steps2).toBe(21366921060721);
   });
 }
