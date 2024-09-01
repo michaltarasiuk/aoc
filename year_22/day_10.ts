1375d999c0403224f02d5186a036c1c6b8c1a5f6
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2024-09-01 17:21:04 +0200

    22/10#1

---
+++ year_22/day_10.ts
@@ -0,0 +1,46 @@
+import {getInputLines} from 'lib/input';
+import {sum} from 'lib/math';
+import {z} from 'zod';
+
+const lines = await getInputLines({year: 2022, day: 10});
+
+type Instruction = z.infer<typeof INSTRUCTION_SCHEMA>;
+
+const INSTRUCTION_SCHEMA = z.union([
+  z.object({op: z.literal('addx'), arg: z.string().transform(Number)}),
+  z.object({op: z.literal('noop')}),
+]);
+
+function parseInstruction(instruction: string): Instruction {
+  const [op, arg] = instruction.split(/\s/);
+  return INSTRUCTION_SCHEMA.parse({op, arg});
+}
+
+function calcSignal(instructions: Instruction[]) {
+  return instructions.reduce((acc, instruction) => {
+    return instruction.op === 'addx' ? acc + instruction.arg : acc;
+  }, 1);
+}
+
+const instructions = lines.flatMap((line): Instruction[] => {
+  const instruction = parseInstruction(line);
+
+  return instruction.op === 'addx'
+    ? [{op: 'noop'}, instruction]
+    : [instruction];
+});
+
+const CYCLES = [20, 60, 100, 140, 180, 220];
+const sumOfSignals = sum(
+  ...CYCLES.map(
+    (cycle) => cycle * calcSignal(instructions.slice(0, cycle - 1)),
+  ),
+);
+
+if (import.meta.vitest) {
+  const {test, expect} = import.meta.vitest;
+
+  test('part 1', () => {
+    expect(sumOfSignals).toBe(14540);
+  });
+}
