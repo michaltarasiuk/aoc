d02cfa95f9ef20b8d9ebfcbc269e1ad4d5fa482c
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2024-09-06 10:40:10 +0200

    18/03#2

--- year_18/day_03.ts
+++ year_18/day_03.ts
@@ -10,26 +10,38 @@ function parseClaim(claim: string) {
 }
 
 const claims = lines.map(parseClaim);
+const overlappingClaims = new Set<number>();
 
 const FABRIC_SIZE = 1_000;
 const fabric = [...Array(FABRIC_SIZE)].map(() =>
-  [...Array(FABRIC_SIZE)].map((): number[] => [])
+  [...Array(FABRIC_SIZE)].map(() => new Set<number>())
 );
 
 for (const {id, x, y, width, height} of claims) {
   for (let i = x; i < x + width; i++) {
     for (let j = y; j < y + height; j++) {
-      fabric[i][j].push(id);
+      fabric[i][j].add(id);
+
+      if (fabric[i][j].size > 1) {
+        fabric[i][j].forEach(overlappingClaims.add, overlappingClaims);
+      }
     }
   }
 }
 
-const overlappingClaims = fabric.flat().filter(ids => ids.length > 1).length;
+const overlappingClaimsCount = fabric.flat().filter(ids => ids.size > 1).length;
+const [nonOverlappingClaim] = new Set(claims.map(({id}) => id)).difference(
+  overlappingClaims
+);
 
 if (import.meta.vitest) {
   const {test, expect} = import.meta.vitest;
 
   test('part 1', () => {
-    expect(overlappingClaims).toBe(101469);
+    expect(overlappingClaimsCount).toBe(101469);
+  });
+
+  test('part 2', () => {
+    expect(nonOverlappingClaim).toBe(1067);
   });
 }
