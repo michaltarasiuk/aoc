196426e2719d18ef1d18f95d476515dd004a9f63
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2024-10-25 23:51:15 +0200

    refactor 15/25 naming

--- year_15/day_25.ts
+++ year_15/day_25.ts
@@ -2,18 +2,18 @@ import {getInputInts} from 'lib/input.js';
 
 const [targetRow, targetCol] = await getInputInts({year: 2015, day: 25});
 
-function calculateSteps(row: number, col: number) {
+function calcSteps(row: number, col: number) {
   const diagonalSum = row + col;
   return (diagonalSum * (diagonalSum - 1)) / 2 - row;
 }
 
-function generateNextCode(previousCode = 20151125) {
+function getNextCode(previousCode = 20151125) {
   return (previousCode * 252533) % 33554393;
 }
 
 let code: number | undefined;
-for (let i = 0; i < calculateSteps(targetRow, targetCol); i++) {
-  code = generateNextCode(code);
+for (let i = 0; i < calcSteps(targetRow, targetCol); i++) {
+  code = getNextCode(code);
 }
 
 if (import.meta.vitest) {
