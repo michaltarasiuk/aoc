d955a15aa7e6a3e93d810499f71a1400356f1950
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2024-11-03 14:05:55 +0100

    refactor 22/05

--- year_22/day_05.ts
+++ year_22/day_05.ts
@@ -9,49 +9,45 @@ function parseCrate(crate: string) {
   return emptyCrateRe.test(crate) ? undefined : crate;
 }
 
-type Stacks = ReturnType<typeof parseStacks>;
-
-function parseStacks([...stacks]: string[]) {
-  const ids = stacks.pop() ?? raise('No stack IDs found');
-
-  return Object.fromEntries(
+function createStacks([...input]: string[]) {
+  const ids = input.pop() ?? raise('No stack IDs found');
+  const stacks = Object.fromEntries(
     extractInts(ids).map(id => {
       const i = ids.indexOf(String(id));
-      return [id, stacks.flatMap(crates => parseCrate(crates[i]) ?? [])];
+      return [id, input.flatMap(crates => parseCrate(crates[i]) ?? [])];
     })
   );
+
+  return {
+    move([count, from, to]: number[], fn = (crates: string[]) => crates) {
+      const crates = stacks[from].splice(0, count);
+      stacks[to].unshift(...fn(crates));
+      return this;
+    },
+    toString() {
+      return Object.values(stacks)
+        .map(([crate]) => crate)
+        .join('');
+    },
+  };
 }
 
-function stacksToString(stacks: Stacks) {
-  return Object.values(stacks)
-    .map(([crate]) => crate)
-    .join('');
-}
-
-function rearrangeStacks(
-  stacks: Stacks,
-  instructions: number[][],
-  fn = (crates: string[]) => crates
-) {
-  return instructions.reduce((acc, [count, from, to]) => {
-    const crates = acc[from].splice(0, count);
-
-    acc[to].unshift(...fn(crates));
-    return acc;
-  }, structuredClone(stacks));
-}
-
-const parsedStacks = parseStacks(stacks);
 const parsedInstructions = instructions.map(extractInts);
 
-const serializedStacks = stacksToString(
-  rearrangeStacks(parsedStacks, parsedInstructions, crates =>
-    crates.toReversed()
+const serializedStacks = parsedInstructions
+  .reduce(
+    (stacks, instruction) =>
+      stacks.move(instruction, crates => crates.toReversed()),
+    createStacks(stacks)
   )
-);
-const serializedStacks2 = stacksToString(
-  rearrangeStacks(parsedStacks, parsedInstructions)
-);
+  .toString();
+
+const serializedStacks2 = parsedInstructions
+  .reduce(
+    (stacks, instruction) => stacks.move(instruction),
+    createStacks(stacks)
+  )
+  .toString();
 
 if (import.meta.vitest) {
   const {test, expect} = import.meta.vitest;
