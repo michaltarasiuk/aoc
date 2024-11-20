03788c6d81986368c8d4c798e935fbee20e2bdd6
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2024-11-20 18:32:19 +0100

    refactor 15/07

--- year_15/day_07.ts
+++ year_15/day_07.ts
@@ -9,7 +9,6 @@ function parseInstruction(instruction: string) {
     ...(instruction.match(/([a-z]|[0-9])+/g) ?? []),
   ];
 }
-
 const circuit = lines.reduce<Record<string, string[]>>((acc, l) => {
   const instruction = parseInstruction(l);
   const dest = instruction.pop();
@@ -19,42 +18,36 @@ const circuit = lines.reduce<Record<string, string[]>>((acc, l) => {
   return acc;
 }, {});
 
-let cache = new Map<string, number>();
-
-function calcSignalOrParse(s: string) {
+function calcSignalOrParse(s: string, cache: Map<string, number>): number {
   const parsed = Number(s);
-
   if (Number.isNaN(parsed)) {
-    return cache.get(s) ?? cache.set(s, calcSignal(s)).get(s)!;
+    return cache.get(s) ?? cache.set(s, calcSignal(s, cache)).get(s)!;
   }
   return parsed;
 }
-
-function calcSignal(dest: string): number {
+function calcSignal(dest: string, cache = new Map<string, number>()): number {
   if (!(dest in circuit)) {
     throw new Error(`No destination for "${dest}"`);
   }
-
   const [gate, a, b] = circuit[dest];
   switch (gate) {
     case 'AND':
-      return calcSignalOrParse(a) & calcSignalOrParse(b);
+      return calcSignalOrParse(a, cache) & calcSignalOrParse(b, cache);
     case 'OR':
-      return calcSignalOrParse(a) | calcSignalOrParse(b);
+      return calcSignalOrParse(a, cache) | calcSignalOrParse(b, cache);
     case 'LSHIFT':
-      return calcSignalOrParse(a) << calcSignalOrParse(b);
+      return calcSignalOrParse(a, cache) << calcSignalOrParse(b, cache);
     case 'RSHIFT':
-      return calcSignalOrParse(a) >> calcSignalOrParse(b);
+      return calcSignalOrParse(a, cache) >> calcSignalOrParse(b, cache);
     case 'NOT':
-      return ~calcSignalOrParse(a);
+      return ~calcSignalOrParse(a, cache);
     default:
-      return calcSignalOrParse(gate);
+      return calcSignalOrParse(gate, cache);
   }
 }
 
 const wireASignal = calcSignal('a');
-cache = new Map([['b', wireASignal]]);
-const finalWireASignal = calcSignal('a');
+const finalWireASignal = calcSignal('a', new Map([['b', wireASignal]]));
 
 if (import.meta.vitest) {
   const {test, expect} = import.meta.vitest;
