de3d945f0ad2786d6b69e2bf90490780259b238f
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2024-12-02 10:51:24 +0100

    24/02#2

--- year_24/day_02.ts
+++ year_24/day_02.ts
@@ -17,12 +17,17 @@ function adjacentLevelsDifferByOneToThree(reports: number[]) {
     return [level - l, r - level].map(Math.abs).every(d => d >= 1 && d <= 3);
   });
 }
+function isSafeReport(report: number[]) {
+  return isSorted(report) && adjacentLevelsDifferByOneToThree(report);
+}
 
 const reportRe = /\d+/g;
 const reports = lines.map(l => l.matchAll(reportRe).map(Number).toArray());
 
-const safeReports = reports.filter(
-  report => isSorted(report) && adjacentLevelsDifferByOneToThree(report)
+const safeReports = reports.filter(isSafeReport);
+const safeReports2 = reports.filter(report =>
+  report.map((_, i, arr) => arr.toSpliced(i, 1)).some(isSafeReport)
 );
 
 assert.strictEqual(safeReports.length, 359, 'Part 1 failed');
+assert.strictEqual(safeReports2.length, 418, 'Part 2 failed');
