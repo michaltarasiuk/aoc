fc4d0f60047bd65929891e5190b267fe820ed6fa
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2024-12-08 18:19:00 +0100

    refactor: rename Antennas type to AntennaPositions and update related variables for clarity

--- year_24/day_08.ts
+++ year_24/day_08.ts
@@ -4,9 +4,9 @@ import {getInputGrid} from 'lib/input.js';
 
 const grid = await getInputGrid({year: 2024, day: 8});
 
-type Antennas = Record<string, [number, number][]>;
+type AntennaPositions = Record<string, [number, number][]>;
 
-function* createAntennaPairs(antennas: Antennas) {
+function* createAntennaPairs(antennas: AntennaPositions) {
   for (const positions of Object.values(antennas)) {
     for (const [x1, y1] of positions) {
       for (const [x2, y2] of positions) {
@@ -19,7 +19,7 @@ function* createAntennaPairs(antennas: Antennas) {
   }
 }
 
-const antennas = grid.keys().reduce<Antennas>((acc, y) => {
+const antennaPositions = grid.keys().reduce<AntennaPositions>((acc, y) => {
   grid[y].forEach((frequency, x) => {
     if (/\w/.test(frequency)) {
       acc[frequency] ??= [];
@@ -29,26 +29,26 @@ const antennas = grid.keys().reduce<Antennas>((acc, y) => {
   return acc;
 }, {});
 
-const antinodesPart1 = new Set<string>();
-for (const [x1, y1, x2, y2] of createAntennaPairs(antennas)) {
+const antinodes = new Set<string>();
+for (const [x1, y1, x2, y2] of createAntennaPairs(antennaPositions)) {
   const i = x1 + (x1 - x2);
   const j = y1 + (y1 - y2);
   if (grid[j]?.[i]) {
-    antinodesPart1.add(`${i},${j}`);
+    antinodes.add(`${i},${j}`);
   }
 }
 
-const antinodesPart2 = new Set<string>();
-for (const [x1, y1, x2, y2] of createAntennaPairs(antennas)) {
-  antinodesPart2.add(`${x1},${y1}`);
+const antinodes2 = new Set<string>();
+for (const [x1, y1, x2, y2] of createAntennaPairs(antennaPositions)) {
+  antinodes2.add(`${x1},${y1}`);
   let i = x1 + (x1 - x2);
   let j = y1 + (y1 - y2);
   while (grid[j]?.[i]) {
-    antinodesPart2.add(`${i},${j}`);
+    antinodes2.add(`${i},${j}`);
     i += x1 - x2;
     j += y1 - y2;
   }
 }
 
-assert.strictEqual(antinodesPart1.size, 381, 'Part 1 failed');
-assert.strictEqual(antinodesPart2.size, 1184, 'Part 2 failed');
+assert.strictEqual(antinodes.size, 381, 'Part 1 failed');
+assert.strictEqual(antinodes2.size, 1184, 'Part 2 failed');
