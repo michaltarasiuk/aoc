741ceb7a4fa345f9e483e5bd6cfe3aa9233a1904
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2024-12-11 20:39:52 +0100

    refactor: simplify regex construction for extractIntegers function

--- lib/parse.ts
+++ lib/parse.ts
@@ -1,4 +1,4 @@
 export function extractIntegers(s: string, {negative} = {negative: true}) {
-  const re = new RegExp(String.raw`${negative ? '-?' : ''}\d+`, 'g');
+  const re = new RegExp((negative ? '-?' : '') + String.raw`\d+`, 'g');
   return s.match(re)?.map(Number) ?? [];
 }
