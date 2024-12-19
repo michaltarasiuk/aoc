5143df8a9bb32fa3f14ee73fc33c6e55b636eef3
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2024-12-19 21:20:05 +0100

    24/19#2

--- year_24/day_19.ts
+++ year_24/day_19.ts
@@ -2,27 +2,27 @@ import assert from 'node:assert';
 
 import {getInputParagraphs} from 'lib/input.js';
 
-const [[patterns], desings] = await getInputParagraphs({year: 2024, day: 19});
+const [[patterns], designs] = await getInputParagraphs({year: 2024, day: 19});
 
-function isDesignValid(
+function countWays(
   design: string,
   patterns: RegExp[],
-  cache: Record<typeof design, boolean> = {}
+  cache: Record<typeof design, number> = {}
 ) {
-  if (design === '') return true;
+  if (design === '') return 1;
   if (design in cache) return cache[design];
-  for (const pattern of patterns) {
-    if (
-      pattern.test(design) &&
-      isDesignValid(design.replace(pattern, ''), patterns, cache)
-    ) {
-      return (cache[design] = true);
-    }
+  let ways = 0;
+  for (const pattern of patterns.filter(p => p.test(design))) {
+    ways += countWays(design.replace(pattern, ''), patterns, cache);
   }
-  return (cache[design] = false);
+  return (cache[design] = ways);
 }
 
-const parsedPatterns = patterns.split(', ').map(p => new RegExp('^' + p));
-const validDesigns = desings.filter(d => isDesignValid(d, parsedPatterns));
+const compiledPatterns = patterns.split(', ').map(p => new RegExp('^' + p));
+const waysCount = designs.map(d => countWays(d, compiledPatterns));
 
-assert.strictEqual(validDesigns.length, 338, 'Part 1 failed');
+const possibleDesignsCount = waysCount.filter(c => c > 0).length;
+const totalWaysCount = waysCount.reduce((acc, c) => acc + c);
+
+assert.strictEqual(possibleDesignsCount, 338, 'Part 1 failed');
+assert.strictEqual(totalWaysCount, 841533074412361, 'Part 2 failed');
