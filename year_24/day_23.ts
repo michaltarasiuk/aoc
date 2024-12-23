35c2d6318c19ff29fa1c686ea3545f0c516568f8
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2024-12-23 23:40:53 +0100

    24/23#1

---
+++ year_24/day_23.ts
@@ -0,0 +1,44 @@
+import assert from 'node:assert';
+
+import {getInputLines} from 'lib/input.js';
+
+const lines = await getInputLines({year: 2024, day: 23});
+
+function* permuteConnections(
+  connections: string[],
+  arrayLength: number
+): Generator<string[]> {
+  if (arrayLength === 0) {
+    yield [];
+    return;
+  }
+  for (const connection of connections) {
+    for (const arr of permuteConnections(connections, arrayLength - 1)) {
+      yield [connection, ...arr];
+    }
+  }
+}
+
+const connections = lines
+  .map(l => l.split('-'))
+  .reduce<Record<string, Set<string>>>((acc, [a, b]) => {
+    (acc[a] ??= new Set()).add(b);
+    (acc[b] ??= new Set()).add(a);
+    return acc;
+  }, {});
+
+const setsOfThree = new Set<string>();
+for (const connection of Object.keys(connections)) {
+  for (const [a, b] of permuteConnections([...connections[connection]], 2)) {
+    if (connections[a].has(b)) {
+      setsOfThree.add([connection, a, b].toSorted().join('-'));
+    }
+  }
+}
+
+const setsContainingT = setsOfThree
+  .values()
+  .filter(s => /t\w/.test(s))
+  .toArray().length;
+
+assert.strictEqual(setsContainingT, 1423, 'Part 1 failed');
