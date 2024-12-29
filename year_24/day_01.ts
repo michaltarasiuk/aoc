c3b06e21bccabc227c0d6e7f9b5d691714f7891e
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2024-12-29 16:10:50 +0100

    fix: refactor location parsing and improve distance and similarity score calculations

--- year_24/day_01.ts
+++ year_24/day_01.ts
@@ -6,23 +6,22 @@ import {frequencies} from 'lib/iterable.js';
 
 const lines = await getInputLines({year: 2024, day: 1});
 
-let {l = [], r = []} = Object.groupBy(
-  lines.flatMap(ln => {
-    const locationPairRe = /^(\d+) {3}(\d+)$/;
-    const [, l, r] = locationPairRe.exec(ln) ?? raise(`Invalid pair: ${ln}`);
-    return [Number(l), Number(r)];
-  }),
-  (_, i) => (i % 2 === 0 ? 'l' : 'r')
+const locations = lines.map(ln => {
+  const locationPairRe = /^(\d+) {3}(\d+)$/;
+  const [, l, r] = locationPairRe.exec(ln) ?? raise(`Invalid pair: ${ln}`);
+  return [Number(l), Number(r)] as const;
+});
+
+const l = locations.map(([l]) => l).sort();
+const r = locations.map(([, r]) => r).sort();
+
+const totalDistance = l.reduce((acc, v, i) => acc + Math.abs(v - r[i]), 0);
+
+const rFrequencies = frequencies(r);
+const similarityScore = l.reduce(
+  (acc, v) => acc + v * (rFrequencies.get(v) ?? 0),
+  0
 );
-(l = l.toSorted()), (r = r.toSorted());
-
-const totalDistance = l
-  .map((n, i) => Math.abs(n - r[i]))
-  .reduce((a, b) => a + b);
-
-const similarityScore = l
-  .map(n => n * (frequencies(r).get(n) ?? 0))
-  .reduce((a, b) => a + b);
 
 assert.strictEqual(totalDistance, 3508942, 'Part 1 failed');
 assert.strictEqual(similarityScore, 26593248, 'Part 2 failed');
