a8023cd5c063a611237f522e10f2a0c244fd1768
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2024-12-29 14:34:19 +0100

    fix: refactor update order validation and improve rule processing logic

--- year_24/day_05.ts
+++ year_24/day_05.ts
@@ -2,31 +2,28 @@ import assert from 'node:assert';
 
 import {getInputParagraphs} from 'lib/input.js';
 
-const paragraphs = await getInputParagraphs({year: 2024, day: 5});
+const [rawRules, rawUpdates] = await getInputParagraphs({year: 2024, day: 5});
 
 type Rules = typeof rules;
 
 function isUpdateInCorrectOrder(rules: Rules, update: number[]) {
   const seen = new Set<number>();
-  for (const pageNumber of update) {
-    for (const [preceding] of rules[pageNumber] ?? []) {
-      if (update.includes(preceding) && !seen.has(preceding)) {
-        return false;
-      }
-    }
-    seen.add(pageNumber);
-  }
-  return true;
+  return update.every(page => {
+    seen.add(page);
+    return (rules[page] ?? []).every(
+      ([preceding]) => !update.includes(preceding) || seen.has(preceding)
+    );
+  });
 }
 function findRule(rules: Rules, preceding: number, succeeding: number) {
   return rules[succeeding]?.find(rule => rule[0] === preceding);
 }
 
 const rules = Object.groupBy(
-  paragraphs[0].map(rule => rule.split('|').map(Number)),
+  rawRules.map(rule => rule.split('|').map(Number)),
   ([, succeeding]) => succeeding
 );
-const updates = paragraphs[1].map(update => update.split(',').map(Number));
+const updates = rawUpdates.map(update => update.split(',').map(Number));
 
 const {correctlyOrdered = [], incorrectlyOrdered = []} = Object.groupBy(
   updates,
@@ -36,13 +33,14 @@ const {correctlyOrdered = [], incorrectlyOrdered = []} = Object.groupBy(
       : 'incorrectlyOrdered'
 );
 
-const sumMiddleCorrect = correctlyOrdered.reduce(
-  (acc, update) => acc + update[Math.floor(update.length / 2)],
-  0
-);
+const sumMiddleCorrect = correctlyOrdered
+  .map(update => update[Math.floor(update.length / 2)])
+  .reduce((acc, page) => acc + page, 0);
+
 const sumMiddleIncorrect = incorrectlyOrdered
   .map(update => update.toSorted((a, b) => (findRule(rules, a, b) ? -1 : 1)))
-  .reduce((acc, update) => acc + update[Math.floor(update.length / 2)], 0);
+  .map(update => update[Math.floor(update.length / 2)])
+  .reduce((acc, page) => acc + page, 0);
 
 assert.strictEqual(sumMiddleCorrect, 7365, 'Part 1 failed');
 assert.strictEqual(sumMiddleIncorrect, 5770, 'Part 2 failed');
