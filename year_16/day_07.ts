7f60c164040fe694c9e81bd6b7c202b8fe1d256d
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2025-01-10 00:04:59 +0100

    refactor: rename supernets and hypernets variables for consistency

--- year_16/day_07.ts
+++ year_16/day_07.ts
@@ -9,24 +9,21 @@ const abaRe = /(\w)((?!\1)\w)\1.* .*\2\1\2.*/;
 
 const ipAddresses = lines.map(l => {
   const squareBracketRe = /\[|\]/;
-  const {supernets = [], hypernets = []} = Object.groupBy(
+  const {supers = [], hypers = []} = Object.groupBy(
     l.split(squareBracketRe),
-    (_, i) => (i % 2 === 0 ? 'supernets' : 'hypernets')
+    (_, i) => (i % 2 === 0 ? 'supers' : 'hypers')
   );
-
-  return {supernets: supernets.join(), hypernets: hypernets.join()};
+  return {supers: supers.join(), hypers: hypers.join()};
 });
 
 const tlsSupportedIpsCount = ipAddresses
-  .map(({supernets, hypernets}) =>
-    Number(abbaRe.test(supernets) && !abbaRe.test(hypernets))
-  )
+  .map(({supers, hypers}) => abbaRe.test(supers) && !abbaRe.test(hypers))
+  .map(Number)
   .reduce((a, b) => a + b);
 
 const sslSupportedIpsCount = ipAddresses
-  .map(({supernets, hypernets}) =>
-    Number(abaRe.test(supernets + ' ' + hypernets))
-  )
+  .map(({supers, hypers}) => abaRe.test(supers + ' ' + hypers))
+  .map(Number)
   .reduce((a, b) => a + b);
 
 assert.strictEqual(tlsSupportedIpsCount, 110, 'Part 1 failed');
