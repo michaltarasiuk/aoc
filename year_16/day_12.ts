0500dc42ef7f544ba9bba8e2275e9035d6a5575d
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2025-01-13 00:47:36 +0100

    16/12#2

--- year_16/day_12.ts
+++ year_16/day_12.ts
@@ -5,31 +5,45 @@ import {isKeyOf} from 'lib/predicate.js';
 
 const lines = await getInputLines({year: 2016, day: 12});
 
-const instructions = lines.map(l => l.split(/\s/));
-const registers = {a: 0, b: 0, c: 0, d: 0};
-
-let instructionPointer = 0;
-while (instructionPointer < instructions.length) {
-  const [op, x, y] = instructions[instructionPointer];
-  switch (op) {
-    case 'cpy':
-      assert(isKeyOf(registers, y));
-      registers[y] = isKeyOf(registers, x) ? registers[x] : Number(x);
-      break;
-    case 'inc':
-      assert(isKeyOf(registers, x));
-      registers[x]++;
-      break;
-    case 'dec':
-      assert(isKeyOf(registers, x));
-      registers[x]--;
-      break;
-    case 'jnz':
-      if ((isKeyOf(registers, x) ? registers[x] : Number(x)) !== 0) {
-        instructionPointer += Number(y) - 1;
-      }
+function execute(
+  registers: Record<'a' | 'b' | 'c' | 'd', number>,
+  ...instructions: string[][]
+) {
+  let instructionPointer = 0;
+  while (instructionPointer < instructions.length) {
+    const [op, x, y] = instructions[instructionPointer];
+    switch (op) {
+      case 'cpy':
+        assert(isKeyOf(registers, y));
+        registers[y] = isKeyOf(registers, x) ? registers[x] : Number(x);
+        break;
+      case 'inc':
+        assert(isKeyOf(registers, x));
+        registers[x]++;
+        break;
+      case 'dec':
+        assert(isKeyOf(registers, x));
+        registers[x]--;
+        break;
+      case 'jnz':
+        if ((isKeyOf(registers, x) ? registers[x] : Number(x)) !== 0) {
+          instructionPointer += Number(y) - 1;
+        }
+    }
+    instructionPointer++;
   }
-  instructionPointer++;
+  return registers;
 }
 
-assert.strictEqual(registers.a, 317993, 'Part 1 failed');
+const instructions = lines.map(l => l.split(/\s/));
+
+assert.strictEqual(
+  execute({a: 0, b: 0, c: 0, d: 0}, ...instructions).a,
+  317993,
+  'Part 1 failed'
+);
+assert.strictEqual(
+  execute({a: 0, b: 0, c: 1, d: 0}, ...instructions).a,
+  9227647,
+  'Part 2 failed'
+);
