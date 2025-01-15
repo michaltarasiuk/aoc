71125bca7f1289fee576863326ccba963af1f629
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2025-01-15 21:22:04 +0100

    refactor: simplify instruction execution by using a helper function and improve register validation

--- year_15/day_23.ts
+++ year_15/day_23.ts
@@ -2,62 +2,59 @@ import assert from 'node:assert';
 
 import {raise} from 'lib/assert.js';
 import {getInputLines} from 'lib/input.js';
+import {isKeyOf} from 'lib/predicate.js';
 
 const lines = await getInputLines({year: 2015, day: 23});
 
-const instructionRe = /(\w+|[+-]\d+)/g;
 const instructions = lines.map(
-  l => l.match(instructionRe) ?? raise('Invalid instruction')
+  l => l.match(/(\w+|[+-]\d+)/g) ?? raise('Invalid instruction')
 );
 
-const initialRegisters = [
-  {a: 0, b: 0},
-  {a: 1, b: 0},
-];
-for (const registers of initialRegisters) {
+function executeInstructions(registers: {a: number; b: number}) {
   let instructionPointer = 0;
   while (instructionPointer < instructions.length) {
     const [operation, ...operands] = instructions[instructionPointer];
+    const reg = operands[0];
+    const offset = Number(operands[1]);
+
     switch (operation) {
       case 'hlf':
-        assert(operands[0] === 'a' || operands[0] === 'b');
-        registers[operands[0]] /= 2;
+        assert(isKeyOf(registers, reg));
+        registers[reg] /= 2;
         instructionPointer++;
         break;
       case 'tpl':
-        assert(operands[0] === 'a' || operands[0] === 'b');
-        registers[operands[0]] *= 3;
+        assert(isKeyOf(registers, reg));
+        registers[reg] *= 3;
         instructionPointer++;
         break;
       case 'inc':
-        assert(operands[0] === 'a' || operands[0] === 'b');
-        registers[operands[0]] += 1;
+        assert(isKeyOf(registers, reg));
+        registers[reg] += 1;
         instructionPointer++;
         break;
       case 'jmp':
         instructionPointer += Number(operands[0]);
         break;
       case 'jie':
-        assert(operands[0] === 'a' || operands[0] === 'b');
-        if (registers[operands[0]] % 2 === 0) {
-          instructionPointer += Number(operands[1]);
-        } else {
-          instructionPointer++;
-        }
+        assert(isKeyOf(registers, reg));
+        instructionPointer += registers[reg] % 2 === 0 ? offset : 1;
         break;
       case 'jio':
-        assert(operands[0] === 'a' || operands[0] === 'b');
-        if (registers[operands[0]] === 1) {
-          instructionPointer += Number(operands[1]);
-        } else {
-          instructionPointer++;
-        }
+        assert(isKeyOf(registers, reg));
+        instructionPointer += registers[reg] === 1 ? offset : 1;
         break;
       default:
         raise('Invalid instruction');
     }
   }
 }
+
+const initialRegisters = [
+  {a: 0, b: 0},
+  {a: 1, b: 0},
+];
+initialRegisters.forEach(executeInstructions);
 
 assert.strictEqual(initialRegisters[0].b, 184, 'Part 1 failed');
 assert.strictEqual(initialRegisters[1].b, 231, 'Part 2 failed');
