c9dd307f19da919dafc37d8698aa1b1f025d8bf4
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2025-01-15 20:28:58 +0100

    15/17#2

--- year_15/day_17.ts
+++ year_15/day_17.ts
@@ -8,21 +8,29 @@ function* permuteContainers(
   liters: number,
   ...containers: number[]
 ): Generator<number[]> {
-  for (const [i, container] of containers.entries()) {
-    if (container === liters) {
-      yield [container];
+  for (const [i, c] of containers.entries()) {
+    if (c === liters) {
+      yield [c];
     } else {
       for (const permuted of permuteContainers(
-        liters - container,
+        liters - c,
         ...containers.slice(i + 1)
       )) {
-        yield [container, ...permuted];
+        yield [c, ...permuted];
       }
     }
   }
 }
 
 const Liters = 150;
+
 const combinations = [...permuteContainers(Liters, ...lines.map(Number))];
+const combinationsByLength = Object.groupBy(combinations, c => c.length);
+const containerCounts = Object.keys(combinationsByLength).map(Number);
 
 assert.strictEqual(combinations.length, 1304, 'Part 1 failed');
+assert.strictEqual(
+  combinationsByLength[Math.min(...containerCounts)]?.length,
+  18,
+  'Part 2 failed'
+);
