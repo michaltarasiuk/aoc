00d478a8bec3e5bc86588604b38fa7ea0cd47706
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2025-01-24 16:11:54 +0100

    19/01#2

--- year_19/day_01.ts
+++ year_19/day_01.ts
@@ -4,8 +4,22 @@ import {getInputInts} from 'lib/input.js';
 
 const ns = await getInputInts({year: 2019, day: 1});
 
-const fuelRequirementsSum = ns
-  .map(n => Math.floor(n / 3) - 2)
+function calcFuel(mass: number) {
+  return Math.floor(mass / 3) - 2;
+}
+function calcFuelRecursive(mass: number): number {
+  const fuel = calcFuel(mass);
+  if (calcFuel(mass) <= 0) {
+    return 0;
+  } else {
+    return fuel + calcFuelRecursive(fuel);
+  }
+}
+
+const fuelRequirementsSum = ns.map(calcFuel).reduce((a, b) => a + b);
+const fuelRequirementsSumRecursive = ns
+  .map(calcFuelRecursive)
   .reduce((a, b) => a + b);
 
 assert.strictEqual(fuelRequirementsSum, 3273715, 'Part 1 failed');
+assert.strictEqual(fuelRequirementsSumRecursive, 4907702, 'Part 2 failed');
