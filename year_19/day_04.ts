557481b41bfdc9258f7d2a88f971c65eccdaede0
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2025-01-27 23:58:08 +0100

    19/04#2

--- year_19/day_04.ts
+++ year_19/day_04.ts
@@ -7,12 +7,22 @@ const [start, end] = await getInputInts(
   {negative: false}
 );
 
-let passwordsCount = 0;
+let part1PasswordsCount = 0;
+let part2PasswordsCount = 0;
 for (let i = start; i <= end; i++) {
   const s = String(i);
-  if (/(\d)\1/.test(s) && s === [...s].sort().join('')) {
-    passwordsCount++;
+  if (!(s === [...s].sort().join(''))) {
+    continue;
+  }
+  const groups = s.match(/(\d)\1+/g);
+  if (groups === null) {
+    continue;
+  }
+  part1PasswordsCount++;
+  if (groups.some(g => g.length === 2)) {
+    part2PasswordsCount++;
   }
 }
 
-assert.strictEqual(passwordsCount, 1686, 'Part 1 failed');
+assert.strictEqual(part1PasswordsCount, 1686, 'Part 1 failed');
+assert.strictEqual(part2PasswordsCount, 1145, 'Part 2 failed');
