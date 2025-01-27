b2e86bdfc788273616439d83f9c49708e1e4bb8f
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2025-01-27 17:48:24 +0100

    refactor: implement lanternfish growth simulation function and update assertions

--- year_21/day_06.ts
+++ year_21/day_06.ts
@@ -4,10 +4,23 @@ import {getInputInts} from 'lib/input.js';
 
 const lanternfishes = await getInputInts({year: 2021, day: 6});
 
-for (let i = 1; i <= 80; i++) {
-  for (const [j, fish] of [...lanternfishes].entries()) {
-    lanternfishes[j] = fish === 0 ? (lanternfishes.push(8), 6) : fish - 1;
+function simulateLanternfishGrowth([...fishTimers]: number[], days: number) {
+  for (let day = 1; day <= days; day++) {
+    const spawningFish = fishTimers.shift()!;
+    fishTimers[6] += spawningFish;
+    fishTimers[8] = spawningFish;
   }
+  return fishTimers.reduce((total, count) => total + count, 0);
 }
 
-assert.strictEqual(lanternfishes.length, 385391, 'Part 1');
+const timers = Array<number>(9).fill(0);
+for (const fish of lanternfishes) {
+  timers[fish]++;
+}
+
+assert.strictEqual(simulateLanternfishGrowth(timers, 80), 385391, 'Part 1');
+assert.strictEqual(
+  simulateLanternfishGrowth(timers, 256),
+  1728611055389,
+  'Part 2'
+);
