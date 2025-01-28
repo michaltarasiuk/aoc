7dba6b637f0d205c8310b6c006ecc550f7a936ab
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2025-01-28 22:17:08 +0100

    refactor: enhance rule resolution logic by adding type definitions and improving function parameters

--- year_20/day_19.ts
+++ year_20/day_19.ts
@@ -4,6 +4,8 @@ import {getInputParagraphs} from 'lib/input.js';
 
 const [rules, messages] = await getInputParagraphs({year: 2020, day: 19});
 
+type Rule = ReturnType<typeof parseRule>;
+
 function extractSingleChar(s: string) {
   return s.match(/^"(\w)"$/)?.[1];
 }
@@ -12,26 +14,21 @@ function parseSubRules(s: string) {
 }
 function parseRule(rule: string) {
   const [id, body] = rule.split(': ');
-  return [id, extractSingleChar(body) ?? parseSubRules(body)] as const;
+  return [Number(id), extractSingleChar(body) ?? parseSubRules(body)] as const;
 }
 
-function resolveRule(rule: string | number[][]): string {
+function resolveRule(rule: Rule[1], ruleMap: Map<number, Rule[1]>): string {
   if (typeof rule === 'string') {
     return rule;
   }
   const subRuleRe = rule
-    .map(subRule =>
-      subRule
-        .map(String)
-        .map(id => resolveRule(ruleMap.get(id)!))
-        .join('')
-    )
+    .map(sub => sub.map(id => resolveRule(ruleMap.get(id)!, ruleMap)).join(''))
     .join('|');
   return `(${subRuleRe})`;
 }
 
 const ruleMap = new Map(rules.map(parseRule));
-const ruleZeroRe = new RegExp(`^${resolveRule(ruleMap.get('0')!)}$`);
+const ruleZeroRe = new RegExp(`^${resolveRule(ruleMap.get(0)!, ruleMap)}$`);
 
 assert.strictEqual(
   messages.filter(m => ruleZeroRe.test(m)).length,
