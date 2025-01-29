e07a787217ba11a489a1d5781a79490ddcb797e0
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2025-01-29 17:44:03 +0100

    refactor: optimize instruction parsing and streamline program execution logic

--- year_20/day_08.ts
+++ year_20/day_08.ts
@@ -4,37 +4,33 @@ import {getInputLines} from 'lib/input.js';
 
 const lines = await getInputLines({year: 2020, day: 8});
 
-function runProgram(...instructions: {op: string; arg: number}[]) {
-  let acc = 0;
-  let ip = 0;
-  const seen = new Set<number>();
+const instructions = lines
+  .map(l => l.split(/\s/))
+  .map(([op, arg]) => ({op, arg: Number(arg)}));
 
-  while (true) {
-    if (seen.has(ip)) {
-      break;
-    } else {
-      seen.add(ip);
-    }
+const seen = new Set<number>();
+let acc = 0;
+let ip = 0;
 
-    const {op, arg} = instructions[ip];
-    switch (op) {
-      case 'acc':
-        acc += arg;
-        ip++;
-        break;
-      case 'jmp':
-        ip += arg;
-        break;
-      case 'nop':
-        ip++;
-        break;
-    }
+while (true) {
+  if (seen.has(ip)) {
+    break;
+  } else {
+    seen.add(ip);
   }
-  return acc;
+  const {op, arg} = instructions[ip];
+  switch (op) {
+    case 'acc':
+      acc += arg;
+      ip++;
+      break;
+    case 'jmp':
+      ip += arg;
+      break;
+    case 'nop':
+      ip++;
+      break;
+  }
 }
-
-const acc = runProgram(
-  ...lines.map(l => l.split(/\s/)).map(([op, arg]) => ({op, arg: Number(arg)}))
-);
 
 assert.strictEqual(acc, 1782, 'Part 1 failed');
