1778020659c5812c271ad785e417c942a7470aca
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2025-01-29 23:45:39 +0100

    16/05#2

--- year_16/day_05.ts
+++ year_16/day_05.ts
@@ -12,9 +12,23 @@ function md5(binaryLike: crypto.BinaryLike) {
 let password = '';
 for (let i = 0; password.length < 8; i++) {
   const hash = md5(doorId + i);
-  if (hash.startsWith('00000')) {
+  if (/^0{5}/.test(hash)) {
     password += hash[5];
   }
 }
 
+let password2 = '_'.repeat(8);
+for (let i = 0; password2.includes('_'); i++) {
+  const hash = md5(doorId + i);
+  if (!/^0{5}/.test(hash)) {
+    continue;
+  }
+  const [pos, val] = [Number(hash[5]), hash[6]];
+  if (!password2[pos] || password2[pos] !== '_') {
+    continue;
+  }
+  password2 = [...password2].toSpliced(pos, 1, val).join('');
+}
+
 assert.strictEqual(password, 'f97c354d', 'Part 1 failed');
+assert.strictEqual(password2, '863dde27', 'Part 2 failed');
