698fcd8f252b9bbda384b74d8e0d7e2e25ac93a2
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2025-01-30 08:18:27 +0100

    refactor: rename categories to layers for improved clarity and consistency

--- year_23/day_05.ts
+++ year_23/day_05.ts
@@ -4,36 +4,30 @@ import {getInputParagraphs} from 'lib/input.js';
 import {extractInts} from 'lib/parse.js';
 import {isDefined} from 'lib/predicate.js';
 
-const [[seeds], ...categories] = await getInputParagraphs({
+const [[seeds], ...layers] = await getInputParagraphs({
   year: 2023,
   day: 5,
 });
 
-function mapThroughCategories(
-  [...categoryLayers]: number[][][],
-  value: number
-): number {
-  const categoryLayer = categoryLayers.shift();
-  if (!isDefined(categoryLayer)) {
+function mapLayers([...layerGroups]: number[][][], value: number): number {
+  const layerGroup = layerGroups.shift();
+  if (!isDefined(layerGroup)) {
     return value;
   }
-  for (const [destinationStart, sourceStart, rangeLength] of categoryLayer) {
-    if (value >= sourceStart && value < sourceStart + rangeLength) {
-      return mapThroughCategories(
-        categoryLayers,
-        destinationStart + (value - sourceStart)
-      );
+  for (const [destStart, srcStart, length] of layerGroup) {
+    if (value >= srcStart && value < srcStart + length) {
+      return mapLayers(layerGroups, destStart + (value - srcStart));
     }
   }
-  return mapThroughCategories(categoryLayers, value);
+  return mapLayers(layerGroups, value);
 }
 
-const categoryLayers = categories
-  .map(([, ...layer]) => layer)
-  .map(layer => layer.map(l => extractInts(l)));
+const layerGroups = layers
+  .map(([, ...group]) => group)
+  .map(group => group.map(g => extractInts(g)));
 
-const lowestConvertedLocation = Math.min(
-  ...extractInts(seeds).map(seed => mapThroughCategories(categoryLayers, seed))
+const minConverted = Math.min(
+  ...extractInts(seeds).map(seed => mapLayers(layerGroups, seed))
 );
 
-assert.strictEqual(lowestConvertedLocation, 323142486, 'Part 1 failed');
+assert.strictEqual(minConverted, 323142486, 'Part 1 failed');
