493662463e95e9588b76f1a0cf80a794f4c1f30f
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2025-01-31 12:25:15 +0100

    refactor: optimize countOrbits function for improved performance and clarity

--- year_19/day_06.ts
+++ year_19/day_06.ts
@@ -2,24 +2,22 @@ import assert from 'node:assert';
 
 import {raise} from 'lib/assert.js';
 import {getInputLines} from 'lib/input.js';
-import {isDefined} from 'lib/predicate.js';
 
 const lines = await getInputLines({year: 2019, day: 6});
 
-function countOrbits(orbits: Map<string, string>, satellite: string): number {
-  const orbit = orbits.get(satellite);
-  if (!isDefined(orbit)) {
-    return 0;
+function countOrbits(orbits: Map<string, string>, satellite: string) {
+  let count = 0;
+  while ((satellite = orbits.get(satellite)!)) {
+    count++;
   }
-  return 1 + countOrbits(orbits, orbit);
+  return count;
 }
 
 const orbitRe = /^(.*)\)(.*)$/;
-const orbits = new Map<string, string>(
-  lines.map(l => {
-    const [, center, satellite] = orbitRe.exec(l) ?? raise('Invalid orbit');
-    return [satellite, center];
-  })
+const orbits = new Map(
+  lines
+    .map(l => orbitRe.exec(l) ?? raise('Invalid orbit'))
+    .map(([, center, satellite]) => [satellite, center])
 );
 
 const totalOrbits = orbits
