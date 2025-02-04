3e21df3e4bf5108cbc43d7c0670fd12aa572a8dc
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2025-02-04 09:19:57 +0100

    refactor: enhance readability of fullyContainedPairsCount calculation

--- year_22/day_04.ts
+++ year_22/day_04.ts
@@ -5,10 +5,11 @@ import {getInput} from 'lib/input.js';
 const input = await getInput({year: 2022, day: 4});
 
 const fullyContainedPairsCount = input
-  .split(/\n/)
-  .map(l => (l.match(/\d+/g) ?? []).map(Number))
-  .map(([a, b, c, d]) => (a <= c && b >= d) || (c <= a && d >= b))
-  .map(Number)
-  .reduce((a, b) => a + b);
+  .split('\n')
+  .map(l => {
+    const [a, b, c, d] = (l.match(/\d+/g) ?? []).map(Number);
+    return (a <= c && b >= d) || (c <= a && d >= b);
+  })
+  .reduce((acc, cond) => acc + Number(cond), 0);
 
 assert.strictEqual(fullyContainedPairsCount, 532, 'Part 1 failed');
