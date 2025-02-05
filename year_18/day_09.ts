7283294c8a104e37a06576f2b7e5d17e0eb705dd
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2025-02-05 23:41:50 +0100

    18/09#1

---
+++ year_18/day_09.ts
@@ -0,0 +1,20 @@
+import assert from 'node:assert';
+
+import {chunkEvery} from 'lib/chunk_every.js';
+import {frequencies} from 'lib/frequencies.js';
+import {getInput} from 'lib/input.js';
+
+const input = await getInput({year: 2019, day: 8});
+
+const Wide = 25;
+const Tall = 6;
+
+const layers = chunkEvery(chunkEvery(input, Wide), Tall);
+const layerWithFewestZeros = layers
+  .map(l => frequencies(l.flat()))
+  .reduce((acc, l) => (acc.get('0')! < l.get('0')! ? acc : l));
+
+const ones = layerWithFewestZeros.get('1')!;
+const twos = layerWithFewestZeros.get('2')!;
+
+assert.strictEqual(ones * twos, 2032, 'Part 1 failed');
