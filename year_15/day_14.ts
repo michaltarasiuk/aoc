a8f83cd77d5ce88f397560b21a0c6496db68b427
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2025-03-07 23:05:23 +0100

    refactor: remove parseReindeerDescription function and inline regex parsing

--- year_15/day_14.ts
+++ year_15/day_14.ts
@@ -4,12 +4,6 @@ import {readInput} from 'lib/input.js';
 
 const input = await readInput({year: 2015, day: 14});
 
-function parseReindeerDescription(description: string) {
-  const m = (description.match(/\d+/g) ?? []).map(Number);
-  assert(m.length === 3, 'Invalid reindeer description');
-  return m as [number, number, number];
-}
-
 function calcFlyingDistance(
   raceDuration: number,
   flyingTime: number,
@@ -17,13 +11,15 @@ function calcFlyingDistance(
 ) {
   const completeCycles = Math.floor(raceDuration / (flyingTime + restingTime));
   const remainingTime = raceDuration % (flyingTime + restingTime);
-
   return completeCycles * flyingTime + Math.min(flyingTime, remainingTime);
 }
 
 const RaceDuration = 2_503;
 const distances = input.split(/\n/).map(l => {
-  const [speed, flyingTime, restingTime] = parseReindeerDescription(l);
+  const [speed, flyingTime, restingTime] = Array.from(
+    l.matchAll(/\d+/g),
+    Number
+  );
   return speed * calcFlyingDistance(RaceDuration, flyingTime, restingTime);
 });
 
