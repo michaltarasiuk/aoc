215fafdcf5f28b99526656b73db71f491ae06a33
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2025-03-08 00:45:15 +0100

    refactor: simplify code by inlining calculation functions and regex parsing

--- year_15/day_25.ts
+++ year_15/day_25.ts
@@ -4,19 +4,11 @@ import {readInput} from 'lib/input.js';
 
 const input = await readInput({year: 2015, day: 25});
 
-function calcSteps(row: number, col: number) {
-  return ((row + col) * (row + col - 1)) / 2 - row;
-}
-function calcNewCode(oldCode = 20151125) {
-  return (oldCode * 252533) % 33554393;
-}
+const [row, col] = input.match(/\d+/g)!.map(Number);
 
-const inputRe = /row (\d+), column (\d+)/;
-const [, row, col] = (input.match(inputRe) ?? []).map(Number);
-
-let code: number | undefined;
-for (let i = 0; i < calcSteps(row, col); i++) {
-  code = calcNewCode(code);
+let code = 20151125;
+for (let i = 0; i < ((row + col) * (row + col - 1)) / 2 - row; i++) {
+  code = (code * 252533) % 33554393;
 }
 
 assert.strictEqual(code, 9132360, 'Part 1 failed');
