f6bca60fa7f55dacf621d2fdbffc79e7697ef3ca
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2025-03-09 00:44:14 +0100

    17/13#2

--- year_17/day_13.ts
+++ year_17/day_13.ts
@@ -5,17 +5,27 @@ import {raise} from 'lib/raise.js';
 
 const input = await readInput({year: 2017, day: 13});
 
+function isCaught({depth, range}: {depth: number; range: number}, delay = 0) {
+  return (depth + delay) % (2 * range - 2) === 0;
+}
+
 const layerRe = /^(\d+): (\d+)$/;
 const firewallLayers = input.split('\n').map(l => {
   const [, depth, range] = l.match(layerRe) ?? raise('Invalid layer');
   return {depth: Number(depth), range: Number(range)};
 });
 
-let severity = 0;
-for (const {depth, range} of firewallLayers) {
-  if (depth % (2 * range - 2) === 0) {
-    severity += depth * range;
+const severity = firewallLayers.reduce((acc, layer) => {
+  if (isCaught(layer)) {
+    acc += layer.depth * layer.range;
   }
+  return acc;
+}, 0);
+
+let delay = 0;
+while (firewallLayers.some(l => isCaught(l, delay))) {
+  delay++;
 }
 
 assert.strictEqual(severity, 3184, 'Part 1 failed');
+assert.strictEqual(delay, 3878062, 'Part 2 failed');
