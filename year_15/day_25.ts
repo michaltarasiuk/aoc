f7a6d377f845a881d6954af7001012a91c296186
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2025-03-27 20:26:58 +0100

    Refactor code for clarity and error handling in day 25 solution

--- year_15/day_25.ts
+++ year_15/day_25.ts
@@ -1,14 +1,19 @@
 import assert from 'node:assert';
 
 import {readInput} from 'lib/input.js';
+import {raise} from 'lib/raise.js';
 
 const input = await readInput({year: 2015, day: 25});
 
-const [row, col] = input.match(/\d+/g)!.map(Number);
+const [row, col] = input.match(/\d+/g)?.map(Number) ?? raise('Invalid input');
 
-let code = 20151125;
+const StartingCode = 20151125;
+const Multiplier = 252533;
+const Modulo = 33554393;
+
+let code = StartingCode;
 for (let i = 0; i < ((row + col) * (row + col - 1)) / 2 - row; i++) {
-  code = (code * 252533) % 33554393;
+  code = (code * Multiplier) % Modulo;
 }
 
 assert.strictEqual(code, 9132360, 'Part 1 failed');
