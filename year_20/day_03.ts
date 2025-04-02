e26f82d0429f574019cb28b52fe54de26316f843
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2025-04-02 21:06:27 +0200

    20/03#2

--- year_20/day_03.ts
+++ year_20/day_03.ts
@@ -5,18 +5,33 @@ import {readInput} from 'lib/input.js';
 const input = await readInput({year: 2020, day: 3});
 
 const Tree = '#';
-const Slope = [3, 1] as const;
+const Slopes = [
+  [1, 1],
+  [3, 1],
+  [5, 1],
+  [7, 1],
+  [1, 2],
+] as const;
 
-const tobogganMap = input.split('\n').map(([...l]) => l);
-const rowLength = tobogganMap[0].length;
-
-let trees = 0;
-let [x, y] = [0, 0];
-while (y !== tobogganMap.length - 1) {
-  [x, y] = [(x + Slope[0]) % rowLength, y + Slope[1]];
-  if (tobogganMap[y][x] === Tree) {
-    trees++;
+function countTrees(tobogganMap: string[][], slope: (typeof Slopes)[number]) {
+  let trees = 0;
+  let [x, y] = [0, 0];
+  while (y < tobogganMap.length - 1) {
+    [x, y] = [(x + slope[0]) % tobogganMap[0].length, y + slope[1]];
+    if (tobogganMap[y][x] === Tree) {
+      trees++;
+    }
   }
+  return trees;
 }
 
-assert.strictEqual(trees, 169, 'Part 1 failed');
+const tobogganMap = input.split('\n').map(([...l]) => l);
+
+const treesForSlope = countTrees(tobogganMap, Slopes[1]);
+const multipliedTrees = Slopes.reduce(
+  (acc, slope) => acc * countTrees(tobogganMap, slope),
+  1
+);
+
+assert.strictEqual(treesForSlope, 169, 'Part 1 failed');
+assert.strictEqual(multipliedTrees, 7560370818, 'Part 2 failed');
