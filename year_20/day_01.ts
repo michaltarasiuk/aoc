196a35f0e38abf773c7c43ffe0a0e0e930bf143c
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2025-04-02 21:39:37 +0200

    20/01#2

--- year_20/day_01.ts
+++ year_20/day_01.ts
@@ -11,13 +11,21 @@ function hasSumOf2020(...entries: number[]) {
 const expenses = expenseReport.split(/\n/).map(Number);
 
 let productOfTwoEntries = 1;
+let productOfThreeEntries = 1;
 for (const entry1 of expenses) {
   for (const entry2 of expenses) {
     if (hasSumOf2020(entry1, entry2)) {
       productOfTwoEntries = entry1 * entry2;
       break;
     }
+    for (const entry3 of expenses) {
+      if (hasSumOf2020(entry1, entry2, entry3)) {
+        productOfThreeEntries = entry1 * entry2 * entry3;
+        break;
+      }
+    }
   }
 }
 
 assert.strictEqual(productOfTwoEntries, 482811, 'Part 1 failed');
+assert.strictEqual(productOfThreeEntries, 193171814, 'Part 2 failed');
