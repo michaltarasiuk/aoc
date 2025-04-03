70984d94a02e3c86c61d58a5aaa8a2d467027ed3
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2025-04-03 20:44:00 +0200

    18/02#2

--- year_18/day_02.ts
+++ year_18/day_02.ts
@@ -5,11 +5,17 @@ import {readInput} from 'lib/input.js';
 
 const input = await readInput({year: 2018, day: 2});
 
+function mapBoxIdToIndexedChars(boxId: string) {
+  return Array.from(boxId, (char, i) => char + `[${i}]`);
+}
+
+const boxIds = input.split(/\n/);
+
 let countOfTwos = 0;
 let countOfThrees = 0;
-for (const l of input.split(/\n/)) {
+for (const boxId of boxIds) {
   const charCounts = new Map(
-    frequencies(l)
+    frequencies(boxId)
       .entries()
       .map(([char, count]) => [count, char])
   );
@@ -17,6 +23,22 @@ for (const l of input.split(/\n/)) {
   if (charCounts.has(3)) countOfThrees++;
 }
 
+const indexedBoxIds = boxIds.map(mapBoxIdToIndexedChars);
+
+let commonChars = '';
+outer: for (const [i, boxA] of indexedBoxIds.entries()) {
+  for (const boxB of indexedBoxIds.slice(i + 1)) {
+    if (new Set(boxA).difference(new Set(boxB)).size !== 1) {
+      continue;
+    }
+    commonChars = [...new Set(boxA).intersection(new Set(boxB))]
+      .map(([char]) => char)
+      .join('');
+    break outer;
+  }
+}
+
 const checksum = countOfTwos * countOfThrees;
 
 assert.strictEqual(checksum, 5880, 'Part 1 failed');
+assert.strictEqual(commonChars, 'tiwcdpbseqhxryfmgkvjujvza', 'Part 2 failed');
