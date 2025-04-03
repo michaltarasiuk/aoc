63ad5f3a4652d398418bf73fd183f73b94faebf1
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2025-04-03 09:47:10 +0200

    Rename expenseReport variable to input for clarity

--- year_20/day_01.ts
+++ year_20/day_01.ts
@@ -2,13 +2,13 @@ import assert from 'node:assert';
 
 import {readInput} from 'lib/input.js';
 
-const expenseReport = await readInput({year: 2020, day: 1});
+const input = await readInput({year: 2020, day: 1});
 
 function hasSumOf2020(...entries: number[]) {
   return entries.reduce((acc, entry) => acc + entry, 0) === 2020;
 }
 
-const expenses = expenseReport.split(/\n/).map(Number);
+const expenses = input.split(/\n/).map(Number);
 
 let productOfTwoEntries = 1;
 let productOfThreeEntries = 1;
