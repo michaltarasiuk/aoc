2cbd8950b66d1267accbb6de0678d0738d5d36ed
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2025-11-17 22:31:14 +0100

    refactor reindeer parsing and distance calculation for clarity and accuracy

--- year_15/day_14.ts
+++ year_15/day_14.ts
@@ -6,20 +6,23 @@ const input = await fetchInput({year: 2015, day: 14});
 
 const RaceDuration = 2_503;
 
-function parseReindeer(line: string) {
-  return Array.from(line.matchAll(/\d+/g), Number);
+function parseReindeer(r: string) {
+  const reindeer = Array.from(r.matchAll(/\d+/g), Number);
+  assert(reindeer.length === 3);
+  return reindeer as [number, number, number];
 }
 
 function calcDistance(speed: number, flyTime: number, restTime: number) {
-  const cycleTime = flyTime + restTime;
-  const fullCycles = Math.floor(RaceDuration / cycleTime);
-  const leftover = RaceDuration % cycleTime;
-  return speed * (fullCycles * flyTime + Math.min(flyTime, leftover));
+  let distance = 0;
+  for (let second = 1; second <= RaceDuration; second += flyTime + restTime) {
+    distance += speed * Math.min(flyTime, RaceDuration - second);
+  }
+  return distance;
 }
 
 const distances = input
   .split(/\n/)
   .map(parseReindeer)
-  .map(([speed, flyTime, restTime]) => calcDistance(speed, flyTime, restTime));
+  .map(r => calcDistance(...r));
 
 assert.strictEqual(Math.max(...distances), 2640, 'Part 1 failed');
