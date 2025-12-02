709cf74b8d713804bacf57b72dd69782739bdf93
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2025-12-02 10:19:01 +0100

    25/01#1

---
+++ year_25/day_02.ts
@@ -0,0 +1,26 @@
+import assert from 'node:assert';
+
+import {fetchInput} from 'lib/input.js';
+
+const input = await fetchInput({year: 2025, day: 2});
+
+function isValidId(id: string) {
+  return id.slice(0, id.length / 2) === id.slice(id.length / 2);
+}
+
+const ranges = input.split(',').map(r => {
+  const [start, end] = r.split('-');
+  return [Number(start), Number(end)] as const;
+});
+
+let count = 0;
+for (const [start, end] of ranges) {
+  for (let i = start; i <= end; i++) {
+    const id = String(i);
+    if (isValidId(id)) {
+      count += i;
+    }
+  }
+}
+
+assert.strictEqual(count, 43952536386, 'Part 1 failed');
