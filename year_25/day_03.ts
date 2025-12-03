ca788ffd33ac7630bb6f59284505ad2ae03ac090
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2025-12-03 08:37:56 +0100

    25/03#2

--- year_25/day_03.ts
+++ year_25/day_03.ts
@@ -4,14 +4,31 @@ import {fetchInput} from 'lib/input.js';
 
 const input = await fetchInput({year: 2025, day: 3});
 
-function findLargestJoltage(bank: number[]) {
-  const a = Math.max(...bank.slice(0, bank.length - 1));
-  const b = Math.max(...bank.slice(bank.indexOf(a) + 1));
-  return Number(String(a) + String(b));
+function findMaxJoltage(bank: number[], size: number) {
+  let joltage = '';
+  let start = 0;
+  while (size--) {
+    const batteries = bank.slice(start, bank.length - size);
+    const maxBattery = Math.max(...batteries);
+    joltage += maxBattery;
+    start += batteries.indexOf(maxBattery) + 1;
+  }
+  return Number(joltage);
 }
 
 const banks = input.split(/\n/).map(([...b]) => b.map(Number));
 
-const totalJoltage = banks.map(findLargestJoltage).reduce((a, b) => a + b, 0);
+const totalJoltage = banks
+  .map(b => findMaxJoltage(b, 2))
+  .reduce((a, b) => a + b, 0);
+
+const totalJoltageWithTwelveBatteries = banks
+  .map(b => findMaxJoltage(b, 12))
+  .reduce((a, b) => a + b, 0);
 
 assert.strictEqual(totalJoltage, 17095, 'Part 1 failed');
+assert.strictEqual(
+  totalJoltageWithTwelveBatteries,
+  168794698570517,
+  'Part 2 failed'
+);
