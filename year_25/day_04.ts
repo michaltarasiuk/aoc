ecc85466db90a0c9f4e4e83772ffc7ad8f16132f
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2025-12-04 12:37:27 +0100

    25/04#2

--- year_25/day_04.ts
+++ year_25/day_04.ts
@@ -4,9 +4,9 @@ import {fetchInput} from 'lib/input.js';
 
 const input = await fetchInput({year: 2025, day: 4});
 
-const Roll = '@';
-const MaxAdjacentRolls = 4;
-const Directions = [
+const PaperRoll = '@';
+const MaxAdjacentRollsForAccess = 4;
+const AdjacentDirections = [
   [-1, -1],
   [-1, 0],
   [-1, 1],
@@ -17,17 +17,48 @@ const Directions = [
   [1, 1],
 ];
 
+function isPaperRoll(v: string) {
+  return v === PaperRoll;
+}
+
+function isAccessibleByForklift(grid: string[][], x: number, y: number) {
+  const adjacentRollCount = AdjacentDirections.reduce(
+    (count, [i, j]) => count + Number(isPaperRoll(grid[y + j]?.[x + i])),
+    0
+  );
+  return adjacentRollCount < MaxAdjacentRollsForAccess;
+}
+
+function getRolls(grid: string[][]) {
+  const rolls: [x: number, y: number][] = [];
+  for (const y of grid.keys()) {
+    for (const x of grid[y].keys()) {
+      if (isPaperRoll(grid[y][x])) {
+        rolls.push([x, y]);
+      }
+    }
+  }
+  return rolls;
+}
+
 const grid = input.split(/\n/).map(l => [...l]);
+const initialAccessibleRolls = getRolls(grid).filter(r =>
+  isAccessibleByForklift(grid, ...r)
+);
 
-const accessibleRolls = grid
-  .flatMap((r, y) => r.map((v, x) => ({v, x, y})))
-  .filter(({v}) => v === Roll)
-  .filter(({x, y}) => {
-    const adjacentRolls = Directions.reduce(
-      (count, [i, j]) => count + +(grid[y + j]?.[x + i] === Roll),
-      0
-    );
-    return adjacentRolls < MaxAdjacentRolls;
-  });
+let totalRollsRemoved = 0;
+while (true) {
+  const accessibleRolls = getRolls(grid).filter(r =>
+    isAccessibleByForklift(grid, ...r)
+  );
+  if (accessibleRolls.length === 0) {
+    break;
+  }
+  for (const [x, y] of accessibleRolls) {
+    grid[y][x] = '.';
+  }
+  totalRollsRemoved += accessibleRolls.length;
+}
 
-assert.strictEqual(accessibleRolls.length, 1409, 'Part 1 failed');
+assert.strictEqual(initialAccessibleRolls.length, 1409, 'Part 1 failed');
+assert.strictEqual(totalRollsRemoved, 8366, 'Part 2 failed');
