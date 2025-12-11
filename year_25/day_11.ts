d0e46509ea525bf9aa1904a0b94b860df4e21469
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2025-12-11 16:24:45 +0100

    25/11#1

---
+++ year_25/day_11.ts
@@ -0,0 +1,36 @@
+import assert from 'node:assert';
+
+import {fetchInput} from '#lib/input.js';
+
+const input = await fetchInput({year: 2025, day: 11});
+
+const START = 'you';
+const END = 'out';
+
+const deviceConnections = Object.fromEntries(
+  input.split(/\n/).map(l => {
+    const [device, ...outputs] = l.matchAll(/\w+/g).map(([m]) => m);
+    return [device, outputs];
+  })
+);
+
+const visited = new Set([START]);
+const queue = [[START]];
+let pathCount = 0;
+while (queue.length > 0) {
+  const path = queue.shift()!;
+  for (const device of deviceConnections[path.at(-1)!]) {
+    if (device === END) {
+      pathCount++;
+      continue;
+    }
+    const newPath = [...path, device];
+    const pathKey = newPath.join(',');
+    if (!visited.has(pathKey)) {
+      visited.add(pathKey);
+      queue.push(newPath);
+    }
+  }
+}
+
+assert.strictEqual(pathCount, 658, 'Part 1 failed');
