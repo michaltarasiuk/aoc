3abf6a6dff27c584f76b26bf4bc2cfef46ec6c8a
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2025-12-16 11:51:33 +0100

    refactor: rename variables for clarity and improve code structure

--- year_25/day_10.ts
+++ year_25/day_10.ts
@@ -10,44 +10,41 @@ const LIGHT_OFF = '.';
 const EXCLUDE_JOLTAGE_REQUIREMENTS = -1;
 
 const machines = input.split(/\n/).map(l => {
-  const [targetLights, ...buttons] = l
+  const [goalState, ...buttons] = l
     .split(/\s/)
-    .slice(0, EXCLUDE_JOLTAGE_REQUIREMENTS);
+    .slice(0, EXCLUDE_JOLTAGE_REQUIREMENTS)
+    .map(v => v.slice(1, -1));
   return {
-    targetLights: targetLights.slice(1, -1),
-    buttons: buttons.map(parseButton),
+    goalState,
+    buttons: buttons.map(b => b.split(',').map(Number)),
   };
 });
 
-let totalButtonPresses = 0;
-for (const {targetLights, buttons} of machines) {
-  const initialLights = LIGHT_OFF.repeat(targetLights.length);
-  const visited = new Set([initialLights]);
-  const queue = [{lights: initialLights, presses: 0}];
+let totalPresses = 0;
+for (const {goalState, buttons} of machines) {
+  const startState = LIGHT_OFF.repeat(goalState.length);
+  const visited = new Set([startState]);
+  const queue = [{state: startState, pressCount: 0}];
   outer: while (queue.length > 0) {
-    const {lights, presses} = queue.shift()!;
+    const {state, pressCount} = queue.shift()!;
     for (const b of buttons) {
-      const newLights = toggleLights(lights, b);
-      if (newLights === targetLights) {
-        totalButtonPresses += presses + 1;
+      const nextState = toggleLights(state, b);
+      if (nextState === goalState) {
+        totalPresses += pressCount + 1;
         break outer;
-      } else if (!visited.has(newLights)) {
-        visited.add(newLights);
-        queue.push({lights: newLights, presses: presses + 1});
+      } else if (!visited.has(nextState)) {
+        visited.add(nextState);
+        queue.push({state: nextState, pressCount: pressCount + 1});
       }
     }
   }
 }
 
-assert.strictEqual(totalButtonPresses, 484, 'Part 1 failed');
+assert.strictEqual(totalPresses, 484, 'Part 1 failed');
 
-function toggleLights([...lights]: string, button: number[]) {
-  for (const i of button) {
+function toggleLights([...lights]: string, indices: number[]) {
+  for (const i of indices) {
     lights[i] = lights[i] === LIGHT_OFF ? LIGHT_ON : LIGHT_OFF;
   }
   return lights.join('');
-}
-
-function parseButton(b: string) {
-  return b.slice(1, -1).split(',').map(Number);
 }
