75ac1e12deca528eb540051c3ace4894c8c5e19e
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2026-05-01 00:56:20 +0200

    21/03#1

---
+++ year-21/day-03.ts
@@ -0,0 +1,21 @@
+import assert from "node:assert"
+
+import {fetchInput, frequencies, transpose} from "../lib.js"
+
+const input = await fetchInput({year: 2021, day: 3})
+
+const mostCommonBit = (bits: (string | undefined)[]) => {
+  const freq = frequencies(bits)
+  return freq.get("0")! > freq.get("1")! ? "0" : "1"
+}
+
+const toDecimal = (bits: string[]) => parseInt(bits.join(""), 2)
+
+const binaryNumbers = input.split(/\n/).map(line => [...line])
+
+const gammaRate = transpose(binaryNumbers).map(mostCommonBit)
+const epsilonRate = gammaRate.map(bit => (bit === "0" ? "1" : "0"))
+
+const powerConsumption = toDecimal(gammaRate) * toDecimal(epsilonRate)
+
+assert.strictEqual(powerConsumption, 1025636, "Part 1 failed")
